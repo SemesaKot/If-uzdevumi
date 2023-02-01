17ab94f33605b6672d69dbc97aee615d09ad38d1
SemesaKot <122967779+SemesaKot@users.noreply.github.com>
2023-02-01 11:14:16 +0200

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,109 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+/* {
+	int sk1, sk2;
+	cout << "Ievadiet 2 skaitlus" << endl;
+	cin >> sk1 >> sk2;
+	if (sk1 > sk2) {
+
+
+		cout << "pirmais skaitlis ir lielaks" << endl;
+	}
+	if (sk2 > sk1) {
+		cout << "otrais skaitlis ir lielaks" << endl;
+	}
+	if (sk1 < 0) {
+		cout << "pirmais skaitlis negativs" << endl;
+	}
+	if (sk2 < 0) {
+		cout << "otrais skaitlis negativs" << endl;
+	}
+	if (sk1 % 2 == 0) {
+		cout << "pirmais skaitlis ir para" << endl;
+	}
+	if (sk2 % 2 == 0) {
+		cout << "pirmais skaitlis ir para" << endl;
+	}
+
+	return 0;
+	}
+*/;
+
+/*int main() {
+
+	int x;
+	cout << "Uzraksti velamus procentus "<<endl;
+	cin >> x;
+	if (x >= 90) {
+		cout << "Grade A";
+	} if (x >= 80 && x < 90) {
+		cout << "Grade B";
+	}
+	if (x >= 70 && x < 80) {
+		cout << "Grade C";
+	}
+	if (x >= 60 && x < 70) {
+		cout << "Grade D";
+	}
+	if (x >= 40 && x < 60) {
+		cout << "Grade E";
+	}
+	if (x < 40) {
+		cout << "Grade F";
+	}
+}
+*/
+
+
+/*int main() {
+
+	int d1, d2;
+	cout << "Attieciga diena" << endl;
+	cin >> d1;
+	if (d1 == 1) {
+		cout<<"Pirmdiena";	}
+	if (d1 == 2) {
+		cout << "Otrdiena";	}
+	if (d1 == 3) {
+		cout << "Tresdiena";	}
+	if (d1 == 4) {
+		cout << "Ceturtdiena";	}
+	if (d1 == 5) {
+		cout << "Piektdiena";	}
+	if (d1 == 6) {
+		cout << "Sestdiena";	}
+	if (d1 == 7) {
+		cout << "Svetdiena";	}
+	cout << endl << "attiecigs menesis"<<endl;
+	cin >> d2;
+		if (d2 == 1) { cout << "1 - janvari"; }
+		if (d2 == 2) { cout << "2 - februari"; }
+		if (d2 == 3) { cout << "3 - marts"; }
+		if (d2 == 4) { cout << "4 - aprilis"; }
+	    if (d2 == 5) { cout << "5 - majs"; }
+		if (d2 == 6) { cout << "6 - junija"; }
+		if (d2 == 7) { cout << "7 - julija"; }
+		if (d2 == 8) { cout << "8 - august"; }
+		if (d2 == 9) { cout << "9 - septembri"; }
+		if (d2 == 10) { cout << "10 - oktobri"; }
+		if (d2 == 11) { cout << "11 - novembri"; }
+		if (d2 == 12) { cout << "12 - decembri"; }
+	
+
+}*/
+
+int main() {
+	int a, a1, a2;
+	cout << "1 mala";
+	cin >> a;
+	cout << "2 mala";
+	cin >> a1;
+	cout << "3 mala";
+	cin >> a2;
+	if (a == a1 == a2) {
+		cout << "Vienadmalu trijsturis";
+	}
+
+
+}
